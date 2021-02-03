e79ac5962b508b57b3bcd36e8ad587dfbb9d2419
Daniel Knights <danknights95@gmail.com>
2021-02-03 21:43:46 +0000

    Typing fix

--- src/lib/toast.d.ts
+++ src/lib/toast.d.ts
@@ -1,12 +1,22 @@
-import type { ComponentCustomProperties } from 'vue'
-import type { LocalOptions } from './types'
-
-export type Toast = {
-    (text: string, localOptions?: LocalOptions): void
-}
+import type { ComponentCustomProperties, Plugin } from 'vue'
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
         $toast: Toast
     }
+}
+
+declare const DKToast: Plugin
+export default DKToast
+
+interface LocalOptions {
+    slot?: string
+    slotLeft?: string
+    slotRight?: string
+    duration?: number
+    styles?: Record<string, string>
+}
+
+export type Toast = {
+    (text: string, localOptions?: LocalOptions): void
 }
